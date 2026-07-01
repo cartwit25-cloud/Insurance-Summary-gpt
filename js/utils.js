9e122de538d8448dbf012d72b0e6e41b8a777a43
cartwit25-cloud <cartwit25@gmail.com>
2026-07-01 17:48:28 +0800

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,36 @@
 // ===== 新增 OCR 功能開始 =====
 
-function cleanNumber(text){
+function cleanNumber(text)// ===== 新增 OCR 功能開始 =====
+
+function setInputValue(id, value) {
+
+    const input = document.getElementById(id);
+
+    if (!input) {
+
+        console.warn("找不到欄位：", id);
+
+        return;
+
+    }
+
+    input.value = value;
+
+    input.dispatchEvent(
+        new Event("input", {
+            bubbles: true
+        })
+    );
+
+    input.dispatchEvent(
+        new Event("change", {
+            bubbles: true
+        })
+    );
+
+}
+
+// ===== 新增 OCR 功能結束 ====={
 
     return Number(
 
